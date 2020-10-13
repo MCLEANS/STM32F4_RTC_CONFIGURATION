beab1a4928579c5046798ec9233789b4a911d7dd
MCLEANS <jackagwaya@gmail.com>
2020-10-13 11:35:32 +0300

    Added struct to hold the RTC time values

--- RTC.h
+++ RTC.h
@@ -5,6 +5,17 @@
 
 namespace custom_libraries{
 
+//struct to hold the RTC time values
+struct RTC_values{
+  uint16_t year;
+  uint8_t month;
+  uint8_t date;
+  uint8_t day_of_week;
+  uint8_t hour;
+  uint8_t minute;
+  uint8_t second;
+};
+
 class RTC_{
     private:
     private:
